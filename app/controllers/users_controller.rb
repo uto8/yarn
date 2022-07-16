cfa44cdc52aca652d6912c8a1a29e1d4e99347f4
uto8 <rb7e8bfa72xe6ee05se@softbank.ne.jp>
2022-07-16 21:35:02 +0900

    フレンド機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
+  before_action :search
+  def index
+    @q = User.ransack(params[:q])
+    @user_results = @q.result(distinct: true)
+    @check = params[:q]
+    @users = User.page(params[:page]).per(1)
+    @friends = current_user.followings & current_user.followers
+  end
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
@@ -16,7 +24,30 @@ class UsersController < ApplicationController
     end
   end
 
+  def following
+    @title = "フォロー"
+    @user = User.find(params[:id])
+    @users = @user.followings
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "フォロワー"
+    @user = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follow'
+  end
+
+  def search
+    @q = User.ransack(params[:q])
+  end
+
   private
+
+  def set_q
+    @q = User.ransack(params[:q])
+  end
+
   def user_params
     params.require(:user).permit(:name, :email)
   end
