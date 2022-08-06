dddcd9eb88227db8cd3a54de41b26e6e906654f6
uto8 <rb7e8bfa72xe6ee05se@softbank.ne.jp>
2022-08-06 17:30:23 +0900

    友達追加

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,22 +1,32 @@
 class RequestsController < ApplicationController
   def create
-    # @event = Event.find(params[:id])
-    # @event.request(current_user, params[:message])
-    # redirect_to root_path
+    @friends = current_user.followings & current_user.followers
     event = Event.find(params[:id])
-    Request.create(request_params)
+    request = Request.create(request_params)
     flash[:success] = "リクエストしました"
-    redirect_to root_path
+    redirect_to action: :edit, id: request.id
   end
 
-  def new
-    @request =Request.new
+  def edit
+    @request =Request.find(params[:id])
+    @friends = current_user.followings & current_user.followers
+  end
+
+  def update
+    @request = Request.find(params[:id])
+    @friends = current_user.followings & current_user.followers
+    if @request.update(request_params)
+      flash[:success] = "友達を追加しました"
+      redirect_to root_path
+    else
+      flash[:error] = "友達の追加に失敗しました"
+    end
   end
   
   private
   def request_params
     params.permit(:user_id, :event_id, :message,
-      request_users_attributes: [:id,:request_id, :user_id, :_destroy],
+      request_users_attributes: [:id, :request_id, :user_id, :_destroy]
     )
   end
 end
